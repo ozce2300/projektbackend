6766ee01b6517c40c1169cc64ddd4868c15225fa
Özgur Celik <ozce2300@student.miun.se>
2024-07-25 10:08:24 +0200

    first commit

---
+++ install.js
@@ -0,0 +1,20 @@
+const mysql = require ("mysql");
+require("dotenv").config();
+
+const connection = mysql.createConnection({
+    host     : process.env.HOST,
+    port     : process.env.PORT,
+    user     : process.env.USER,
+    password : process.env.PASSWORD,
+    database : process.env.DATABASE,
+  });
+   
+  connection.connect(err =>{
+    if(err) {
+        console.log("Could not connect to database")
+    }
+
+    else {
+        console.log("Connected to database")
+    }
+  });
