969fa9bdb8e622d76b5b99e944a468b278a9ca1d
Özgur Celik <ozce2300@student.miun.se>
2024-07-25 10:30:46 +0200

    Skapat anslutning till databas

--- install.js
+++ install.js
@@ -1,12 +1,14 @@
 const mysql = require ("mysql");
 require("dotenv").config();
 
+//Ansluta till server
+
 const connection = mysql.createConnection({
     host     : process.env.HOST,
     port     : process.env.PORT,
     user     : process.env.USER,
     password : process.env.PASSWORD,
-    database : process.env.DATABASE,
+    database : process.env.DATABASE
   });
    
   connection.connect(err =>{
@@ -18,3 +20,9 @@ const connection = mysql.createConnection({
         console.log("Connected to database")
     }
   });
+
+
+
+
+
+
